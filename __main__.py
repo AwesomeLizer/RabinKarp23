9b6a75c48c32f589013c65f2b69dc620abf97f85
Gleb <kiselev.ga@phystech.edu>
2023-12-13 21:12:24 +0300

    main refreshed

--- __main__.py
+++ __main__.py
@@ -1,34 +1,7 @@
 from fasta import read_fasta
+from newkarp import rabin_karp
 import sys
-from random import randint
 
-
-mod = 2**127 - 1
-base = randint(1, mod)
-
-
-def p_hash(text, base, mod):
-    L_hash = [0]
-    p = 1
-    for sym in text:
-        L_hash.append(((L_hash[-1] * base) % mod + ord(sym)) % mod)
-        p = (p * base) % mod
-    return (L_hash, p)
-
-
-
-def get_loci(text, pattern, base, mod):
-
-    pattern_hash, p = p_hash(pattern, base, mod)
-    pattern_hash = pattern_hash[-1]
-    text_hash = p_hash(text, base, mod)[0]
-    pos = []
-    for left_lim in range(len(text) - len(pattern) + 1):
-        right_lim = left_lim + len(pattern)
-        if pattern_hash == ((text_hash[right_lim] - (text_hash[left_lim] * p) % mod) % mod):
-            pos.append(left_lim)
-
-    return pos
 
 
 
@@ -39,5 +12,5 @@ if __name__ == '__main__':
     pattern = read_fasta(p_fa)[1]
     text = read_fasta(t_fa)[1]
 
-    res = get_loci(text, pattern, base, mod)
+    res = rabin_karp(text, pattern)
     print(*res)
