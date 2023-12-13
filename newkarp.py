b9526e273da9d8fac11f6f7eb13afce8fb7c6928
Gleb <kiselev.ga@phystech.edu>
2023-12-13 21:03:27 +0300

    newkarp refreshed

--- newkarp.py
+++ newkarp.py
@@ -2,18 +2,23 @@ from random import randint
 
 mod = 2**127 - 1
 base = randint(1, mod)
-pos = []
+
 def rabin_karp(s, p):
+    pos = []
     hash_p = 0
     hash_s = 0
-    h = (base ** (len(p))) % mod
+    h = 1
+
     for i in range(len(p)):
         hash_p = (base * hash_p + ord(p[i])) % mod
         hash_s = (base * hash_s + ord(s[i])) % mod
+        h = (h * base) % mod
+
     for i in range(len(s) - len(p) + 1):
         if hash_s == hash_p:
-            if s[i::i+len(p)] == p:
-                pos.append(i)
+            pos.append(i)
+
         if i < len(s) - len(p):
-            hash_s = (base * (hash_s - ord(s[i]) * h) + ord(s[i+len(p)])) % mod
+            hash_s = (hash_s - ord(s[i])*h) % mod
+            hash_s = (base * hash_s + ord(s[i+len(p)])) % mod
     return pos
