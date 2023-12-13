43f1f66d7142e2f6e948d1e229335eda47af1341
Gleb <kiselev.ga@phystech.edu>
2023-12-13 22:04:11 +0300

    hello word

--- __main__.py
+++ __main__.py
@@ -1,7 +1,31 @@
 from fasta import read_fasta
-from newkarp import rabin_karp
 import sys
+from random import randint
 
+mod = 2**127 - 1
+base = randint(1, mod)
+
+def rabin_karp(s, p):
+    pos = []
+    hash_p = 0
+    hash_s = 0
+    h = 1
+
+    for i in range(len(p)):
+        hash_p = (base * hash_p + ord(p[i])) % mod
+        hash_s = (base * hash_s + ord(s[i])) % mod
+    
+    for i in range(len(p) - 1):
+        h = h*base % mod
+
+    for i in range(len(s) - len(p) + 1):
+        if hash_s == hash_p:
+            pos.append(i)
+
+        if i < len(s) - len(p):
+            hash_s = (hash_s - ord(s[i])*h) % mod
+            hash_s = (base * hash_s + ord(s[i+len(p)])) % mod
+    return pos
 
 
 
